5e5db72e3e89004be720ac4cdc63d5a5dacdcc6b
adam askari <34635255+bearownage@users.noreply.github.com>
2023-12-06 01:06:42 -0800

    remove redundant only process 1st frame

--- additive_hash_sound.py
+++ additive_hash_sound.py
@@ -1,54 +1,76 @@
 import wave
 import numpy as np
 import time
+import json
 from media_player_app import MediaPlayerApp
 
-class WindowInfo:
-    def __init__(self, start_frame, end_frame, window_size, start_second, end_second):
-        self.start_frame = start_frame
-        self.end_frame = end_frame
-        self.window_size = window_size
-        self.start_second = start_second
-        self.end_second = end_second
+def get_audio_values(file_path, is_query_wav):
+    audio_values = []
 
-def additive_hash(frame):
-    hash_value = np.sum(frame)
-    return hash_value
+    with wave.open(file_path, 'rb') as wave_file:
+        # Get audio file parameters
+        channels = wave_file.getnchannels()
+        sample_width = wave_file.getsampwidth()
+        frame_rate = wave_file.getframerate()
+        num_frames = wave_file.getnframes()
+        audio_value_to_frame_index_map={}
+        frame_index_to_audio_map={}
 
-def create_hash_value_for_audio_window(wav_file, start_frame, window_size_frames):
-    wav_file.setpos(start_frame)
-    frames = wav_file.readframes(window_size_frames)
-    frames_array = np.frombuffer(frames, dtype=np.int16)
-    hash_value = additive_hash(frames_array)
-    return hash_value
+        # Read all frames
+        frames = wave_file.readframes(num_frames)
 
-def process_wav_file(file_path, window_size, hop_size):
-    windows_dict = {}
-    wav_file = wave.open(file_path, 'rb')
-    channels = wav_file.getnchannels()
-    frame_rate = wav_file.getframerate()
-    sample_width = wav_file.getsampwidth()
-    num_frames = wav_file.getnframes()
-    
-    window_size_frames = int(window_size * frame_rate)
-    print("Window size frames: " + str(window_size_frames))
-    hop_size_frames = int(hop_size * frame_rate)
-    
-    for start_frame in range(0, num_frames-window_size_frames, hop_size_frames):
-        hash_value = create_hash_value_for_audio_window(wav_file, start_frame, window_size_frames)
-        window_info = WindowInfo(start_frame, start_frame + window_size_frames, window_size, start_frame / frame_rate, (start_frame + window_size_frames) / frame_rate)
-        if hash_value in windows_dict:
-            windows_dict[hash_value].append(window_info)
-        else:
-            windows_dict[hash_value] = [window_info]
-    return windows_dict
+        # Convert frames to NumPy array
+        audio_array = np.frombuffer(frames, dtype=np.int16)
 
-        # for hash_value, window_info_list in windows_dict.items():
-        #    for window_info in window_info_list:
-               # print(f"Hash Value: {hash_value}, Start Frame: {window_info.start_frame}, End Frame: {window_info.end_frame}, Window Size: {window_info.window_size}")
+        # Split the array into channels
+        audio_array = audio_array.reshape((-1, channels))
+
+        # Iterate over frames and get audio values
+        frame_num = 0
+        print("Audio array len: " + str(len(audio_array)))
+        for frame in audio_array:
+            frame_num += 1
+            # For mono audio, frame contains a single value
+            # For stereo audio, frame contains two values (left and right)
+            sum = frame[0]
+            if is_query_wav:
+                audio_value_to_frame_index_map[int(sum)] = [frame]
+                frame_index_to_audio_map[frame_num] = int(sum)
+                return audio_value_to_frame_index_map, frame_index_to_audio_map
+
+            frame_index_to_audio_map[frame_num] = sum
+            if sum in audio_value_to_frame_index_map:
+                audio_value_to_frame_index_map[int(sum)].append(frame_num)
+            else:
+                audio_value_to_frame_index_map[int(sum)] = [frame_num]
+
+            # print(frame.tolist())
+            # audio_values.append(frame.tolist())
+
+    return audio_value_to_frame_index_map, frame_index_to_audio_map
+
+if __name__ == "__main__":
+    wav_file_path = './dataset/Videos/Audios/video6.wav'
+    query_file_path = './dataset/Queries/Audios/video6_1.wav'
+    full_video_audio, dummy2 = get_audio_values(wav_file_path, False)
+    dummy , frame_index_to_audio_map_query = get_audio_values(query_file_path, True)
+    start_frame_val = frame_index_to_audio_map_query[1]
+    print(full_video_audio[start_frame_val])
+    # Specify the file path where you want to save the data
+    file_path = "audio_signatures.json"
+
+    # Serialize the dictionary to a JSON string and write it to the file
+    with open(file_path, "w") as file:
+        json.dump(full_video_audio, file)
+
+
+
+"""
 if __name__ == "__main__":
     app = MediaPlayerApp("./dataset/Videos/video6.mp4", 408.125161)
     app.mainloop()
+"""
+
 """
 if __name__ == "__main__":
     wav_file_path = './dataset/Videos/Audios/video6.wav'
@@ -59,7 +81,7 @@ if __name__ == "__main__":
 
     window_values=process_wav_file(wav_file_path, window_size, hop_size)
     query_file = wave.open(query_file_path, 'rb')
-    query_hash_value=create_hash_value_for_audio_window(query_file, 0, query_file.getframerate() * 20)
+    query_hash_value=create_hash_value_for_audio_window(query_file, 0)
     #print("Query: " + str(query_hash_value))
     if query_hash_value in window_values:
         print("Hit!")
