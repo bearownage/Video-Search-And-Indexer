bc051e40c3b2e597e9ba90c76835f78d3a9460cc
adam askari <34635255+bearownage@users.noreply.github.com>
2023-12-06 22:00:56 -0800

    commit before merge

--- additive_hash_sound.py
+++ additive_hash_sound.py
@@ -6,22 +6,25 @@ import json
 from media_player_app import MediaPlayerApp
 
 def get_audio_values(file_path, is_query_wav):
-    audio_values = []
-
     with wave.open(file_path, 'rb') as wave_file:
         # Get audio file parameters
+        frames_per_second = 30
         channels = wave_file.getnchannels()
-        sample_width = wave_file.getsampwidth()
-        frame_rate = wave_file.getframerate()
         num_frames = wave_file.getnframes()
+        frame_rate = wave_file.getframerate()
+        frames_to_skip = int(frame_rate / frames_per_second)
         audio_value_to_frame_index_map={}
         frame_index_to_audio_map={}
+        print("Number of frames:" + str(num_frames))
+        print("Framerate: " + str(wave_file.getframerate()))
 
         # Read all frames
         frames = wave_file.readframes(num_frames)
+        # print(frames)
 
         # Convert frames to NumPy array
         audio_array = np.frombuffer(frames, dtype=np.int16)
+        print("1Audio array len: " + str(len(audio_array)))
 
         # Split the array into channels
         audio_array = audio_array.reshape((-1, channels))
@@ -29,7 +32,8 @@ def get_audio_values(file_path, is_query_wav):
         # Iterate over frames and get audio values
         frame_num = 0
         print("Audio array len: " + str(len(audio_array)))
-        for frame in audio_array:
+        for i in range(0, len(audio_array), frames_to_skip):
+            frame = audio_array[i]
             frame_num += 1
             # For mono audio, frame contains a single value
             # For stereo audio, frame contains two values (left and right)
@@ -55,18 +59,21 @@ if __name__ == "__main__":
     files = os.listdir(path)
     for file in files:
         if os.path.isfile(os.path.join(path, file)):
+            print("Processing file: " + str(file))
             full_video_audio, dummy2 = get_audio_values(os.path.join(path, file), False)
             file_path = str(file) + "_audio_signatures.json"
             with open(file_path, "w") as file:
                 json.dump(full_video_audio, file)
+    query_file_path = './dataset/Queries/Audios/video6_1.wav'
+    query_file_mp4_path = "./dataset/Videos/video6.mp4"
+    dummy1, frame_index_to_audio_map_query = get_audio_values(query_file_path, True)
+    start_frame_val = frame_index_to_audio_map_query[1]
 
-    # wav_file_path = './dataset/Videos/Audios/video6.wav'
-    # query_file_path = './dataset/Queries/Audios/video6_1.wav'
-    # start_frame_val = frame_index_to_audio_map_query[1]
-    # print(full_video_audio[start_frame_val])
-    # Specify the file path where you want to save the data
-
-    # Serialize the dictionary to a JSON string and write it to the file
+    signature_name = query_file_path.split("/")[-1].split("_")[0] + ".wav_audio_signatures.json"
+    file_path = "./" + signature_name
+    with open(file_path, "r") as file:
+        frame_signatures = json.load(file)
+        print(frame_signatures[str(start_frame_val)])
     
-
-    # dummy , frame_index_to_audio_map_query = get_audio_values(query_file_path, True)
+    app = MediaPlayerApp(query_file_mp4_path, float(12241)/30)
+    app.mainloop()
